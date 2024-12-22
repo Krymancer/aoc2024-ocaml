8b255e0d546b905b037f35a3a708a6aacd106795
Júnior Nascimento <krymancer@protonmail.com>
2024-12-22 01:55:20 -0300

    day 01 part 02

--- problems/day01/main.ml
+++ problems/day01/main.ml
@@ -1,12 +1,11 @@
-let part1 input =
-  let lines =
+let get_input filename = In_channel.with_open_bin filename In_channel.input_all
+
+let sanitize_input input =
     input
     |> String.split_on_char '\n'
     |> List.map String.trim
-  in
 
-  let pairs =
-    List.map
+let make_pairs lines = List.map
       (fun line ->
          let parts =
            line
@@ -21,7 +20,10 @@ let part1 input =
             failwith "Each line must contain exactly two integers."
       )
       lines
-  in
+
+let part1 input =
+  let lines = sanitize_input input in
+  let pairs = make_pairs lines in
 
   let left_list, right_list = List.split pairs in
 
@@ -33,10 +35,47 @@ let part1 input =
   let result =  List.fold_left ( + ) 0 diffs in
   string_of_int result
 
-let get_input filename = In_channel.with_open_bin filename In_channel.input_all
+module IntMap = Map.Make(struct
+  type t = int
+  let compare = compare
+end)
+
+let part2 (input : string) : string =
+  let lines = sanitize_input input in
+  let pairs = make_pairs lines in
+
+  let left_list, right_list = List.split pairs in
+
+  let frequency_map =
+    List.fold_left
+      (fun acc num ->
+         let current_count =
+           match IntMap.find_opt num acc with
+           | Some count -> count
+           | None -> 0
+         in
+         IntMap.add num (current_count + 1) acc)
+      IntMap.empty
+      right_list
+  in
+
+  let sum_result =
+    List.fold_left
+      (fun acc num ->
+         let freq =
+           match IntMap.find_opt num frequency_map with
+           | Some count -> count
+           | None -> 0
+         in
+         acc + (num * freq))
+      0
+      left_list
+  in
+
+  string_of_int sum_result
 
 let solve lines = 
-  print_endline (part1 lines)
-
+  print_endline (part1 lines);
+  print_endline (part2 lines)
 
 let () = solve (get_input "problems/day01/input.txt")
